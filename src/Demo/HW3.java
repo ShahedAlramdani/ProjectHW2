7de23275ed504d77e7fe29378cbc558b38847b7c
chuchu <chuchu@rands-air.lan>
2025-01-30 10:36:46 -0500

    created new code

---
+++ src/Demo/HW3.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class HW3 {
+	public class DemoTest3 {
+		public static void main (String args []) {
+			
+			String A = "Alpha";
+			String B = "Boy";
+			
+		
+			System.out.println (A);
+			System.out.println (B);
+
+			
+			
+		}
+	
+	}
+}
